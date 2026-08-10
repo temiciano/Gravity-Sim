6f2120588b730ebb0fe411fef0c697ff6a422257
temiciano <temiciano@gmail.com>
2026-08-10 12:30:20 -0400

    example and planet

--- gravedad.py
+++ gravedad.py
@@ -1,10 +1,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 
-#Esfera
-#Gravedad, masa, posicion
+#Sol
+#gravedad, masa, posicion
 
 Gravity = 1.0
 Mass = 1000.0
-Pos = np.array([0.0],[0.0])
+Pos = np.array([0.0 , 0.0])
+
+#Planeta
+#posicion, velocidad
+
+pos = np.array([10.0 , 0.0])
+vel = np.array([0.0 , 0.0])
 
