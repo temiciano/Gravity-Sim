ce29bed7d4d52dba25c805b14c6500207dfaf5ec
temiciano <temiciano@gmail.com>
2026-08-12 18:39:50 -0400

    start draw

--- gravedad.py
+++ gravedad.py
@@ -17,13 +17,14 @@ sun = Body("Sun", 10000.0, np.array([0.0 , 0.0]), np.array([0.0 , 0.0]))
 planet = Body("Planet", 1000.0, np.array([10.0 , 0.0]), np.array([0.0 , 8.0]))
 #rocket = Body("Rocket", 1.0, np.array([10.0 , 0.0]), np.array([0.0 , 8.0]))
 
+assets = [sun , planet]
 
 #Newton attraction
 
 #Constant universal Gravity
 G = 1
 
-def calc_aceleration(body, attractor):
+def calc_acceleration(body, attractor):
 
 	#Calculate vector distance, then convert vector to num
 	distance = body.pos - attractor.pos
@@ -33,10 +34,13 @@ def calc_aceleration(body, attractor):
 	numerator = G * body.mass * attractor.mass
 
 	denominator = np.power(d , 2)
+	force = numerator / denominator
 
-	Force = (numerator / denominator)
+	magnitude = force / body.mass
+	direction - distance / d
+	acceleratation = magnitude * direction
 
-	return 0
+	return acceleration
 
 #Function Main
 tick = 0.1
@@ -44,3 +48,19 @@ tick = 0.1
 #for i in range
 
 #Draw on screen
+axet = 10
+
+#environment, Cartesian plane
+fig, ax = plt.subplots(figsize=(10 , 10))
+ax.set_xlim(-axet , axet)
+ax.set_ylim(-axet , axet)
+ax.set_aspect('equal')
+ax.set_title('Orbital system')
+
+circles = []
+for asset in assets:
+	colour =
+	size =
+
+
+plt.show()
